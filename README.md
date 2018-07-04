17086c8645c94f5876377d72bf226b574a9e99cb
Owen Buckley <895923+thescientist13@users.noreply.github.com>
2018-07-04 16:25:45 -0400

    chapter 2 (#8)
    * chapter 2
    
    * remove comment

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # random-plot-generator
-An example of using SVG and Custom Elements (with `lit-html`) to create data visualizations and other fun things in JavaScript.  Inspired by [this FunFunFunction video](https://www.youtube.com/watch?v=o98qlvrcqiU) on creating a neural network.  For the time being I will just be emulating the one in the video, but where it goes from there, who knows!
+An example of using SVG and Custom Elements (with `lit-html`) to create data visualizations and other fun things in JavaScript.  Inspired by [this FunFunFunction YouTube series](https://www.youtube.com/playlist?list=PL0zVEGEvSaeGmPpG8tAaqSmf3k56LPwiF) on creating a Neural Network in JavaScript.  For the time being I will just be emulating what's done in the video, but where it goes from there, who knows!
 
 ## Setup
 1. Clone the repo
--- plot-generator.component.js
+++ plot-generator.component.js
@@ -1,6 +1,10 @@
 import { html, render, svg } from './node_modules/lit-html/lit-html.js';
 import { repeat } from './node_modules/lit-html/lib/repeat.js';
 
+function random(high, low) {
+  return Math.random() * (high - low) + low;
+}
+
 class PlotGenerator extends HTMLElement {
   constructor() {
     super();
@@ -9,8 +13,27 @@ class PlotGenerator extends HTMLElement {
     this.NUM_POINTS = 100;
     this.INTERVAL_MS = 500;
     this.points = this.generatePoints(this.NUM_POINTS);
-    this.randomInterval
-    
+
+    const point = {x: 200, y: 400}; 
+    const sample1 = {x: 721, y: 432 };
+    const sample2 = {x: 211, y: 122 };
+    const sample3 = {x: 328, y: 833 };
+    const sample4 = {x: 900, y: 400 };
+    this.randomWeights = {
+      x: random(-1, 1),
+      y: random(-1, 1)
+    }
+
+    this.trainedWeights = this.train(this.randomWeights, sample1, this.team(sample1));
+    this.trainedWeights = this.train(this.trainedWeights, sample2, this.team(sample2));
+    this.trainedWeights = this.train(this.trainedWeights, sample3, this.team(sample3));
+    this.trainedWeights = this.train(this.trainedWeights, sample4, this.team(sample4));
+
+    console.log('randomWeights', this.randomWeights);
+    console.log('control', this.team(point));
+    console.log('train', this.train(this.randomWeights, point, this.team(point))); 
+    console.log('trainedWeights', this.trainedWeights);
+
     this.root = this.attachShadow({ mode: 'closed' });
     render(this.template(), this.root);
   }
@@ -30,6 +53,36 @@ class PlotGenerator extends HTMLElement {
       default:
 
     }
+  }
+
+  train(weights, point, control) {
+    const guessResult = this.guessPlotPosition(weights, point);
+    const error = control - guessResult;
+
+    return {
+      x: weights.x + (point.x * error),
+      y: weights.y + (point.y * error)
+    };
+  }
+
+  // 1 (above / right position)
+  // -1 (below / left position)
+  guessPlotPosition(weights, point) {
+    const sum = point.x * weights.x + point.y * weights.y;
+    const plotPosition = sum >= 0 ? 1 : -1;
+
+    return plotPosition;
+  }
+
+  getTeamColorForPointPosition(point) {
+    // const plotPosition = this.guessPlotPosition(this.randomWeights, point);
+    const plotPosition = this.guessPlotPosition(this.trainedWeights, point);
+
+    return plotPosition === 1 ? 'green' : 'blue';
+  }
+
+  team(point) {
+    return point.x > point.y ? 1 : -1;
   }
 
   randomizePoints() {
@@ -53,7 +106,7 @@ class PlotGenerator extends HTMLElement {
     return svg`
       <svg width="${this.X_MAX}" height="${this.Y_MAX}" style="border: 1px solid #020202">
         ${repeat(this.points, (point) => point.id, (point) => {
-          const color = point.x > point.y ? 'green' : 'blue';
+          const color = this.getTeamColorForPointPosition(point);
 
           return svg`
             <circle 
@@ -67,6 +120,7 @@ class PlotGenerator extends HTMLElement {
           `
         })}
 
+        <!-- TODO why does this has to come _after_ the <circle>s are generated?? -->
         <line x1="0" x2="${this.X_MAX}" y1="0" y2="${this.Y_MAX}" stroke="black"/>
       </svg>
     `;
